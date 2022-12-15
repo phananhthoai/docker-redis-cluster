eb1c801c96f5b6163b285add78dfdac7eea64eb6
khanh <kenkukhanh@gmail.com>
2022-12-15 15:52:51 +0700

    update file

--- common.sh
+++ common.sh
@@ -5,14 +5,14 @@ set -ex
 # Turn on export DEBIAN_FRONTEND=noninteractive if docker run mysql
 #export DEBIAN_FRONTEND=noninteractive
 
-apt update
+sudo apt update
 
-apt install -y systemd systemd-sysv kmod coreutils lsb-release wget curl zip unzip tar busybox iputils-ping iproute2 net-tools jq gnupg2 netcat bind9-dnsutils openssh-client git binutils ripgrep bash-completion
+sudo apt install -y systemd systemd-sysv kmod coreutils lsb-release wget curl zip unzip tar busybox iputils-ping iproute2 net-tools jq gnupg2 netcat bind9-dnsutils openssh-client git binutils ripgrep bash-completion
 
-apt install -y redis-server redis-sentinel
+sudo apt install -y redis-server redis-sentinel
 
-curl https://getmic.ro | bash && mv micro /usr/local/bin
+#curl https://getmic.ro | bash && mv micro /usr/local/bin
 
-sed -i -E 's/^bind [0-9.]+ :+[0-9]$/bind 0.0.0.0/g' /etc/redis/redis.conf
+sudo sed -i -E 's/^bind [0-9.]+ :+[0-9]$/bind 0.0.0.0/g' /etc/redis/redis.conf
 
-sed -i -E 's/^bind [0-9.]+ :+[0-9]$/bind 0.0.0.0/g' /etc/redis/sentinel.conf
+sudo sed -i -E 's/^bind [0-9.]+ :+[0-9]$/bind 0.0.0.0/g' /etc/redis/sentinel.conf
--- scripts/master.sh
+++ scripts/master.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -ex
 
-IP_MASTER=$(ip addr show eth0 | grep 'inet ' | sed -E 's/\s+inet ([0-9.]+)\/[0-9]+ .*/\1/g' | head -n 1)
+NAME_NET=$(ip route list | grep default | grep -Eo 'dev [a-z0-9]+' | awk '{print $2}')
+IP_MASTER=$(ip addr show $NAME_NET | grep 'inet ' | sed -E 's/\s+inet ([0-9.]+)\/[0-9]+ .*/\1/g' | head -n 1)
 REDIS_ROOT_PASS=${REDIS_ROOT_PASS:-password}
 
 # If use docker run mysql no need use service redis-server start
-#service redis-server start
+#sudo service redis-server start
 
 while :;
 do
@@ -17,20 +18,20 @@ do
   fi
 done
 
-sed -i -E 's/^protected-mode no$/protected-mode yes/g' /etc/redis/redis.conf
+sudo sed -i -E 's/^protected-mode no$/protected-mode yes/g' /etc/redis/redis.conf
 #echo 'bind 0.0.0.0' >> /etc/redis/redis.conf
-echo "masterauth \"$REDIS_ROOT_PASS\"" | tee -a /etc/redis/redis.conf
+echo "masterauth \"$REDIS_ROOT_PASS\"" | sudo tee -a /etc/redis/redis.conf
 #echo 'replicaof "%s" 6379' $IP_MASTER >> /etc/redis/redis.conf
-echo "requirepass \"$REDIS_ROOT_PASS\"" | tee -a /etc/redis/redis.conf
+echo "requirepass \"$REDIS_ROOT_PASS\"" | sudo tee -a /etc/redis/redis.conf
 
-service redis-server restart
+sudo service redis-server restart
 
-echo "sentinel monitor redis-master \"$IP_MASTER\" 6379 2" | tee -a /etc/redis/sentinel.conf
-echo 'sentinel down-after-milliseconds redis-master 1500' | tee -a /etc/redis/sentinel.conf
-echo 'sentinel failover-timeout redis-master 3000' | tee -a /etc/redis/sentinel.conf
-echo 'protected-mode no' | tee -a /etc/redis/sentinel.conf
+echo "sentinel monitor redis-master \"$IP_MASTER\" 6379 2" | sudo tee -a /etc/redis/sentinel.conf
+echo 'sentinel down-after-milliseconds redis-master 1500' | sudo tee -a /etc/redis/sentinel.conf
+echo 'sentinel failover-timeout redis-master 3000' | sudo tee -a /etc/redis/sentinel.conf
+echo 'protected-mode no' | sudo tee -a /etc/redis/sentinel.conf
 
-service redis-sentinel restart
+sudo service redis-sentinel restart
 
-echo 'info replication' | redis-cli -a password
-echo 'info sentinel' | redis-cli -p 26379
+echo 'info replication' | sudo redis-cli -a password
+echo 'info sentinel' | sudo redis-cli -p 26379
